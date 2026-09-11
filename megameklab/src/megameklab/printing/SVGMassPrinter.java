b77767e780041d9f5b8999bb19ae549927b73478
exeea <dev@exeea.com>
2026-09-11 19:10:08 +0200

    .

--- megameklab/src/megameklab/printing/SVGMassPrinter.java
+++ megameklab/src/megameklab/printing/SVGMassPrinter.java
@@ -145,7 +145,7 @@ public class SVGMassPrinter {
     private static boolean SKIP_SVG = false; // Set to true to skip SVG generation
     private static boolean SKIP_UNITS = false; // Set to true to skip units generation
     private static boolean SKIP_EQUIPMENT = false; // Set to true to skip equipment generation
-    private static boolean SKIP_UNIT_FILES = false; // Set to true to skip BLK/MTF re-save generation
+    private static boolean SKIP_UNIT_FILES = true; // Set to true to skip BLK/MTF re-save generation
     private static boolean SKIP_DETAILED_CALCULATIONS = true; // Set to true to skip the detailed BV/Cost calculations
     private static final boolean EXPORT_CALCULATION_DETAILS_TO_FILES = true; // Set to true to not embed the detailed BV/Cost calculations into the units.json but in a subfolder keyed by name
     private static boolean EXPORT_CALCULATIONS_AS_TEXT = true;
